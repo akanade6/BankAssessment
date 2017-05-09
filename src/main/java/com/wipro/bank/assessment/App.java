0c981203fcd9492196e2f3fd749e83967f5ba1f1
Rama Lanka <rama.lanka@standardbank.co.za>
2017-05-09 15:11:53 +0200

    Design Completed

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -1,13 +1,35 @@
 package com.wipro.bank.assessment;
 
+import com.wipro.bank.entity.Account;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static final List<Account> accounts = new ArrayList<Account>();
+	private List<Account> emp2;
+
+
+	public Account getAccountDetailsByID(int id)
+	{
+		Account emp1=null;		
+		return emp1;
+	}
+
+	public List<Account> getAccountDetailsByBalance(double amount)
+	{    
+		emp2 =new ArrayList<Account>();          
+		return emp2;
+	}
+
+	public static void main( String[] args )
+	{
+		
+
+	}
 }
