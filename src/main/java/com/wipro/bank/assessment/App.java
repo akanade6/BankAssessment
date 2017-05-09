40dfc3d7564b8973e33b38e4151176270e22ac4b
Rama Lanka <rama.lanka@standardbank.co.za>
2017-05-09 15:16:22 +0200

    Methods added

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -14,18 +14,32 @@ public class App
 	public static final List<Account> accounts = new ArrayList<Account>();
 	private List<Account> emp2;
 
-
-	public Account getAccountDetailsByID(int id)
-	{
-		Account emp1=null;		
-		return emp1;
-	}
-
-	public List<Account> getAccountDetailsByBalance(double amount)
-	{    
-		emp2 =new ArrayList<Account>();          
-		return emp2;
-	}
+	public Account getAccountDetailsByID(int id)    {
+           Account emp1=null;
+           for(Account e:accounts)
+           {
+                  if(e.getAccountID()==id){
+                        emp1=e;
+                  }                  
+           }
+           return emp1;          
+           
+    }
+    
+    public List<Account> getAccountDetailsByBalance(double amount)
+    {      
+    emp2 =new ArrayList<Account>();          
+           for(Account e:accounts){                 
+                  if(e.getBalance()>=amount){                     
+                        emp2.add(e);
+                  }
+           }
+           if(emp2.size()==0){
+                  emp2=null;
+           }
+           return emp2;         
+           
+    }
 
 	public static void main( String[] args )
 	{
--- src/test/java/com/wipro/bank/assessment/AppTest.java
+++ src/test/java/com/wipro/bank/assessment/AppTest.java
@@ -1,15 +1,83 @@
 package com.wipro.bank.assessment;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class AppTest extends TestCase {
+import java.util.ArrayList;
+import java.util.List;
 
-	public void testGetAccountDetailsByID() {
-		//fail("Not yet implemented");
+import org.junit.Before;
+import org.junit.Test;
+
+import com.wipro.bank.entity.Account;
+
+public class AppTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
 	}
 
-	public void testGetAccountDetailsByBalance() {
-		//fail("Not yet implemented");
+	@Test
+	public void testGetAccountDetailsByID() {
+		App.accounts.add(new Account (1,"acc1",101.11));		
+		App.accounts.add(new Account (3,"acc3",303.33));
+		App.accounts.add(new Account (4,"acc4",404.43));
+		App.accounts.add(new Account (5,"acc5",505.55));	
+		assertNull(new App().getAccountDetailsByID(567));
+	}
+	
+//	@Test
+//	public void testGetAccountDetailsByID2() {
+//		App.accounts.add(new Account (1,"acc1",101.11));		
+//		App.accounts.add(new Account (3,"acc3",303.33));
+//		App.accounts.add(new Account (4,"acc4",404.43));
+//		App.accounts.add(new Account (5,"acc5",505.55));	
+//		Account acc=new App().getAccountDetailsByID(2);
+//		assertNotNull(acc);
+//		assertEquals("patel",acc.getUserName());
+//	}
+	
+	@Test
+	public void getAccountDetailsByBalance(){
+		App.accounts.add(new Account (1,"acc1",101.11));		
+		App.accounts.add(new Account (3,"acc3",303.33));
+		App.accounts.add(new Account (4,"acc4",404.43));
+		App.accounts.add(new Account (5,"acc5",505.55));	
+		assertNull("When No account found:",new App().getAccountDetailsByBalance(909.01));
+	}
+	
+	@Test
+	public void getAccountDetailsByBalance2(){
+		App.accounts.add(new Account (1,"acc1",101.11));		
+		App.accounts.add(new Account (3,"acc3",303.33));
+		App.accounts.add(new Account (4,"acc4",404.43));
+		App.accounts.add(new Account (5,"acc5",505.55));	
+		List<Account> actualAccounts=new App().getAccountDetailsByBalance(0.0);
+		assertNotNull(actualAccounts);
+		assertEquals(App.accounts.size(),actualAccounts.size());
+	}
+	
+	@Test
+	public void getAccountDetailsByBalance3(){
+		App.accounts.add(new Account (1,"acc1",101.11));		
+		App.accounts.add(new Account (3,"acc3",303.33));
+		App.accounts.add(new Account (4,"acc4",404.43));
+		App.accounts.add(new Account (5,"acc5",505.55));	
+		double amount=201.01;
+		
+		List<Account> checkActualAccounts=new ArrayList<Account>();		
+		for(Account account:App.accounts){			
+			if(account.getBalance()>=amount){				
+				checkActualAccounts.add(account);
+			}
+		}
+		if(checkActualAccounts.size()==0){
+			checkActualAccounts=null;
+		}
+		
+		List<Account> actualAccounts=new App().getAccountDetailsByBalance(amount);
+		assertNotNull(actualAccounts);
+		assertEquals(checkActualAccounts.size(),actualAccounts.size());
 	}
 
 }
